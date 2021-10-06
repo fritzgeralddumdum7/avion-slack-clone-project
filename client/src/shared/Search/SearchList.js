36a491aeaef7a5433fc5685576b4ffdb96206602
idlehands08 <geofftd08@gmail.com>
2021-10-06 11:04:08 +0800

    Create Channel Functionality and UI

--- client/src/shared/Search/SearchList.js
+++ client/src/shared/Search/SearchList.js
@@ -1,12 +1,12 @@
 import SearchListItem from './SearchListItem'
 import './SearchList.scoped.css';
 
-function SearchList ({ results, searched, customClass }) {
+function SearchList ({ results, searched, customClass, handleClick, isNavLink }) {
 
     return (
         <ul>
-          { results.map((item, index) => item.name.toLowerCase().includes(searched.toLowerCase()) && 
-          <SearchListItem key={index} item={item} customClass={customClass} />) }
+          { results && results.map((item, index) => item.email.toLowerCase().includes(searched.toLowerCase()) && 
+          <SearchListItem key={index} item={item} customClass={customClass} handleClick={handleClick} isNavLink={isNavLink}/>) }
           <li>Not the results you expected? <a href="#">Give feedback</a> or <a href="#">learn more</a></li>
         </ul>
     )
--- client/src/shared/Search/SearchListItem.js
+++ client/src/shared/Search/SearchListItem.js
@@ -2,15 +2,28 @@ import { NavLink } from "react-router-dom";
 import Image from '../Image/Image';
 import './SearchListItem.scoped.css';
 
-function SearchListItem ({ item, customClass }) {
+function SearchListItem ({ item, customClass, handleClick, isNavLink=true }) {
     const imgStyle = { height: '20px', width: '20px', marginRight: '10px', borderRadius: '4px' }
+
     return (
-    <li className={customClass}>
-        <NavLink className="nav-link" to='/'>
-            <Image source={ item.image } customStyle={ imgStyle } />
-            { item.name }
-        </NavLink>
-    </li>
+        <div>
+            {
+            (isNavLink) ? 
+            <li className={customClass}>         
+                <NavLink className="nav-link" to='/' >
+                    <Image source={ item.image } customStyle={ imgStyle } />
+                   {item.email}
+                </NavLink> 
+            </li>
+            :                          
+            <li className={customClass} onClick={() => handleClick(item)} >                
+                <div className="div-list-item" onClick={() => handleClick(item)} >
+                    <Image source={ item.image } customStyle={ imgStyle } />
+                    {item.email}
+                </div> 
+            </li>
+            }
+        </div>
     )
 }
 
