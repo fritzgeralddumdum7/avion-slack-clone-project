6caa748cae3de48e93572d1fae86fb3048d75222
Sean Dela Cruz <andreadlacrz1@gmail.com>
2021-10-11 07:27:42 +0800

    logintest

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Login from '../Login';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it ("Renders correctly", () => {
+    const {queryByTestId, queryByPlaceholderText} = render(<Login />)
+    expect(queryByTestId("submit-btn")).toBeTruthy()
+    expect(queryByPlaceholderText('Password', 'name@work-email.com')).toBeTruthy()
+})
+
+describe("Input value", () => {
+    it("updates on change", () => {
+        const {queryByPlaceholderText} = render(<Login />)
+
+        const logInput = queryByPlaceholderText('Password', 'name@work-email.com');
+
+        fireEvent.change(logInput, {target: {value: "test"}})
+
+        expect(logInput.value).toBe("test")
+    })
+})
+
+describe("Submit with Data", () => {
+    it("triggers requestLogin", () => {
+        const requestLogin = jest.fn();
+
+        const {queryByTestId, queryByPlaceholderText} = render(<Login requestLogin={requestLogin} />)
+        const logInput = queryByPlaceholderText('Password', 'name@work-email.com');
+        fireEvent.change(logInput, {target: {value: "test"}})
+        
+        fireEvent.click(queryByTestId('submit-btn'))
+        expect(requestLogin, logInput).toHaveBeenCalled();
+    })
+  })
+
+describe("Submit Button", () => {
+    describe ("empty query", () => {
+        it("does not trigger", () => {
+        const requestLogin = jest.fn();
+            const {queryByTestId} = render(<Login requestLogin={requestLogin} />)
+
+            fireEvent.click(queryByTestId('submit-btn'))
+            expect(requestLogin).not.toHaveBeenCalled()
+        })
+    })
+})
