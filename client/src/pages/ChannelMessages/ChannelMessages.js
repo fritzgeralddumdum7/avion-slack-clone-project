c973242ce776b420a72825fcde028dcadbdd0580
sun-asterisk <fritzgeralddumdum@github.com>
2021-10-17 17:29:27 +0800

    fix channelinfo undefined

--- client/src/pages/ChannelMessages/ChannelMessages.js
+++ client/src/pages/ChannelMessages/ChannelMessages.js
@@ -43,6 +43,7 @@ function ChannelMessages () {
     const [showMemberList, setShowMemberList] = useState(false);
     const [memberList, setMemberList] = useState([]);
     const [usersNotOnChannel, setUsersNotOnChannel] = useState([]);
+    const [totalMembers, setTotalMembers] = useState(0);
 
     const socket = useRef();
 
@@ -54,28 +55,32 @@ function ChannelMessages () {
     useEffect(() => {
         // gets the info of users not yet a member of the channel and stores it on usersNotOnChannel useState.
         const setNonMembersInfo = (channelMembers) => {
-            let memberIds = channelMembers.map(member => member.user_id)
-    
-            users.forEach(user => {
-                if (!memberIds.includes(user.id)) {
-                    setUsersNotOnChannel(previous => [...previous, user])
-                }
-            })
+            if (channelMembers) {
+                let memberIds = channelMembers.map(member => member.user_id)
+        
+                users.forEach(user => {
+                    if (!memberIds.includes(user.id)) {
+                        setUsersNotOnChannel(previous => [...previous, user])
+                    }
+                })
+            }
         }
 
         // gets the info of channelMembers from users and stores it on memberList useState.
         const setUserInfoOnMemberList = (channelMembers) => {
-            channelMembers.forEach(member => {
-                const user = users.find(user => user.id === member.user_id)
-                setMemberList(previous => [...previous, user]);
-            })
+            if (channelMembers) {
+                console.log(channelMembers)
+                channelMembers.forEach(member => {
+                    const user = users.find(user => user.id === member.user_id)
+                    setMemberList(previous => [...previous, user]);
+                })
+            }
         }
         
-        if (isFetchingChannel) {
-            if (!isFetchingChannel) {
-                setUserInfoOnMemberList(channelInfo.channel_members);
-                setNonMembersInfo(channelInfo.channel_members);
-            }
+        setUserInfoOnMemberList(channelInfo.channel_members);
+        setNonMembersInfo(channelInfo.channel_members);
+        if (channelInfo.channel_members) {
+            setTotalMembers(channelInfo.channel_members.length)
         }
     }, [isFetchingChannel, users, channelInfo])
 
@@ -155,7 +160,7 @@ function ChannelMessages () {
        <div className="message-container container full-content d-flex flex-column justify-bottom" style={{ gap: '20px', paddingTop: '0px', paddingLeft: '0px' ,paddingRight: '0px' }}>
             <PageHeader 
                 title={channelInfo.name} 
-                buttonLabel={`Members ${channelInfo.channel_members.length}`}  
+                buttonLabel={`Members ${totalMembers}`}  
                 handleButtonClick={handleShowMemberListModal}
             />
              { 
