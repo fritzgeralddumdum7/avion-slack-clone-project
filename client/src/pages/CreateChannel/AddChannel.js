a3624a1a0e01b46dd6a1b078355e5b8f31946f7c
sun-asterisk <fritzgeralddumdum@github.com>
2021-10-18 11:58:53 +0800

    modify some ui in channel create modal

--- client/src/pages/CreateChannel/AddChannel.js
+++ client/src/pages/CreateChannel/AddChannel.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import OutsideClickHandler from 'react-outside-click-handler';
-import {
-    fetchOwnedChannels
-} from '../../redux/users';
 
 import SearchList from '../../shared/Search/SearchList';
 import Input from '../../shared/Input/Input';
@@ -14,43 +11,35 @@ import ChannelApi from '../../api/ChannelApi';
 import './AddChannel.scoped.css';
 
 function AddChannel ({closeAddChannel}) {
+    const { users } = useSelector(state => state.users);
     
     const [searched, setSearched] = useState('');
-    const [results, setResults] = useState([]);
     const [channelName, setChannelName] = useState('');
+    const [results, setResults] = useState([]);
     const [uidList, setUidList] = useState([]);
     const [usersList, setUsersList] = useState([]);
     const [nextClicked, setNextClicked] = useState(false);
     const [error, setError] = useState(null);
-    const { 
-        users
-    } = useSelector(state => state.users);
-    const dispatch = useDispatch();
 
     useEffect(() => {
         setResults(users);
      }, [users]);
 
-     //handles Create Channel button. Triggers api create channel function passing in the channel name and chosen channel members. 
+     // handles Create Channel button. Triggers api create channel function passing in the channel name and chosen channel members. 
     const createChannel = async () => {
-        
-        if (uidList.length < 1) {
-            setError('Please input users you want to include in your channel');
-        }
-
         const payload = {
           'name': channelName,
           'user_ids': uidList
         };
+
         await ChannelApi.create(payload)
         .then(res => {
-             //updates redux channel global state to update the user owned channel list on sidebar component
+            // updates redux channel global state to update the user owned channel list on sidebar component
             if(res.data.errors) {
                 setError(res.data.errors)
                 setNextClicked(false);
             } else {
-                 dispatch(fetchOwnedChannels());
-                 window.location = `../channel/${res.data.data.id}`
+                window.location = `../channel/${res.data.data.id}`
             }
         })
         .catch(error => console.log(error.response.data.data.errors))
@@ -59,40 +48,44 @@ function AddChannel ({closeAddChannel}) {
         setUidList([]);
     }
     
-    //gets the text value from search input
+    // gets the text value from search input
     const handleOnChange = (e) => {
         setSearched(e.target.value);
     }
 
-    //gets the value of channel name from the channel name text input
+    // gets the value of channel name from the channel name text input
     const handleChannelNameChange = (e) => {
         setChannelName(e.target.value);
     }
 
     const handleNext = () => {
-        if(channelName.trim()==='') {
+        if(!channelName.trim()) {
             setError('Must input a channel name');
         }
-        else if(channelName.length< 4) {
-             setError('Name too short (minimum of 4 characters)');
+        else if(channelName.length < 4) {
+            setError('Name too short (minimum of 4 characters)');
         }
         else {
             setNextClicked(true);
             setError(null);
         }
-
     }
 
     const handleBack = () => {
-        setChannelName('');
         setNextClicked(false);
     }
 
-    //handles choosing of channel members from our search list. 
-    //adds the users onto usersList state and their ids to the uidList state which will be used for sending the info to slack api
+    // handles choosing of channel members from our search list. 
+    // adds the users onto usersList state and their ids to the uidList state which will be used for sending the info to slack api
     const handleClick = (item) => {
-        if(uidList.length<10) {
-            setUsersList([...usersList, {id: item.id, image: item.image, email: item.email}]);
+        if(uidList.length < 10) {
+            setUsersList([...usersList, {
+                id: item.id, 
+                image: 
+                item.image, 
+                email: 
+                item.email
+            }]);
             setUidList([...uidList, item.id]);
             setResults(results.filter(result => result.id !== item.id));
             setSearched('');
@@ -144,7 +137,7 @@ function AddChannel ({closeAddChannel}) {
                     <div className="flex-column wrapper-full-width">
                         <div className='wrapper-full-width wrapper-search-input'>
                             <Input
-                                label='Search for users to add to channel'
+                                label='Search for users to add to channel - optional'
                                 value={searched}
                                 handleChange={handleOnChange}
                                 placeholder={'@somebody, or somebody@example.com'}
@@ -167,7 +160,11 @@ function AddChannel ({closeAddChannel}) {
                             </div>
                         }
                         <div className="wrapper-full-width container-list">
-                            <List customClass={'add-channel-list'}list={usersList} removeItem={removeItem}/> 
+                            <List 
+                                customClass={'add-channel-list'}
+                                list={usersList} 
+                                removeItem={removeItem}
+                            /> 
                         </div>
                         <div className='wrapper-button d-flex wrapper-full-width'>
                             <button onClick={handleBack}>Back</button>
@@ -182,9 +179,7 @@ function AddChannel ({closeAddChannel}) {
     return (
         <div className="wrapper-blackout-backdrop">
                 <OutsideClickHandler onOutsideClick={closeAddChannel}>
-               
                     {nextClicked ? addChannelMembers() : channelNameInput()}
-                      
                 </OutsideClickHandler>
         </div>
                  
